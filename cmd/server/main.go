a0ab9eb5b7fd0463fe5d3567aa8e0be25ca01c7a
Timofey Bardenkov <tdenkov123@mail.ru>
2026-09-11 18:56:08 +0300

    fix Body.Close()

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -74,7 +74,7 @@ func checkLive() error {
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("healthz: %s", resp.Status)
